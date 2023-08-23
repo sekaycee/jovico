28b6cd189158b0c6c7bd5b327b9a34a95d254f7e
Kay Cee <sekaycee@gmail.com>
2023-08-23 07:42:52 +0100

    fix: edit author schema

--- jovico-sanity/schemas/index.ts
+++ jovico-sanity/schemas/index.ts
@@ -1,3 +1,6 @@
 import page from './documents/page'
+import post from './documents/post'
+import author from './documents/author'
 
-export const schemaTypes = [page]
+
+export const schemaTypes = [author, page, post]
