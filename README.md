8a4f5e3dd413ab8a36c7fa4e9c7168f8fe9202e4
Kay Cee <sekaycee@gmail.com>
2023-08-23 06:40:33 +0100

    feat: create new sanity project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Jovico Bikes
---
+++ jovico-sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '9346fe9i',
+    dataset: 'production'
+  }
+})
